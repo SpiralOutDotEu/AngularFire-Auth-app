052019dd0eae3b11d1e594a63f69c6603679a41a
Spiral Out <spiralout.eu@gmail.com>
2018-04-03 22:16:27 +0300

    Test user & guest nav visibility according AuthService.authenticated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,37 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import {GuestNavComponent} from './guest-nav/guest-nav.component';
-import {UserNavComponent} from './user-nav/user-nav.component';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {Component} from '@angular/core';
+import {AuthService} from './core/auth.service';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
+@Component({
+  selector: 'app-guest-nav',
+  template: ''
+})
+class MockGuestNavComponent {
+}
+
+@Component({
+  selector: 'app-user-nav',
+  template: ''
+})
+class MockUserNavComponent {
+}
+
+let mockAuthState: BehaviorSubject<boolean>;
+mockAuthState = new BehaviorSubject(false);
+
+
+class MockAuthService {
+  authenticated() {
+    return mockAuthState.asObservable(); }
+}
+
+let component: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+let authService: AuthService;
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -11,25 +40,40 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        GuestNavComponent,
-        UserNavComponent
+        MockGuestNavComponent,
+        MockUserNavComponent
       ],
-    }).compileComponents();
+      providers: [
+        {provide: AuthService, useClass: MockAuthService}
+      ]
+    }).compileComponents().then(() => {
+      mockAuthState.next(true);
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      authService = TestBed.get(AuthService);
+    });
   }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+
+  it('should show only user nav bar for authenticated user', fakeAsync(() => {
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    mockAuthState.next(true);
+    tick();
+    fixture.detectChanges();
+    expect(compiled.querySelector('app-user-nav')).not.toBe(null);
+    expect(compiled.querySelector('app-guest-nav')).toBe(null);
+  }));
+
+  it('should show only guest nav bar for unauthenticated user', fakeAsync(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    mockAuthState.next(false);
+    tick();
+    fixture.detectChanges();
+    expect(compiled.querySelector('app-user-nav')).toBe(null);
+    expect(compiled.querySelector('app-guest-nav')).not.toBe(null);
   }));
 });
