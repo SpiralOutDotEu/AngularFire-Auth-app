c0d540ba6fc49fe968a20b33ffb6f5aa5f821302
Spiral Out <spiralout.eu@gmail.com>
2018-04-15 22:44:03 +0300

    Firebase deploy with TravisCI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAhym2EjS7oPG-F71QHA2sNVKkXDI-qVuA',
+    authDomain: 'authentication-test-c82d5.firebaseapp.com',
+    databaseURL: 'https://authentication-test-c82d5.firebaseio.com',
+    projectId: 'authentication-test-c82d5',
+    storageBucket: 'authentication-test-c82d5.appspot.com',
+    messagingSenderId: '858840711803'
+  }
 };
